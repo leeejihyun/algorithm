6578ba663c3a571090975744fbdbd07039563c37
leeejihyun97 <>
2022-01-02 10:16:45 +0000

    Update 큰-수의-법칙.py

--- 이코테/part02/chapter03/큰-수의-법칙.py
+++ 이코테/part02/chapter03/큰-수의-법칙.py
@@ -4,29 +4,10 @@ N, M, K = map(int, input().split())
 # 입력받고 배열 생성
 array = list(map(int, input().split()))
 
-# 가장 큰 수 확인
-maxNum = max(array)
+array.sort()
+maxNum = array[N-1] # 가장 큰 수 확인
+nextMaxNum = array[N-2] # 두 번째로 큰 수 확인
 
-# 가장 큰 수가 몇 개 있는지 확인
-maxNumCnt = 0
-for num in array:
-    if num == maxNum:
-        maxNumCnt += 1
-
-# 1개면 다음으로 큰 수 확인
-answer = 0
-cnt = 0
-if maxNumCnt == 1:
-    array.remove(maxNum)
-    nextMaxNum = max(array)
-    # print(nextMaxNum)
-    while cnt < M:
-        for j in range(K):
-            answer += maxNum
-            cnt += 1
-        answer += nextMaxNum
-        cnt += 1
-elif maxNumCnt >= 2:
-    answer = maxNum * M
+answer = (maxNum * K + nextMaxNum) * (M // (K+1)) + maxNum * (M % (K+1))
 
 print(answer)
