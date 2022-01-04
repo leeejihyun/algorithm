41d7b07199adfa304db78c6a60e04e71474683f1
leeejihyun97 <>
2022-01-04 12:42:08 +0000

    Update 1이-될-때까지.py

--- 이코테/part02/chapter03/1이-될-때까지.py
+++ 이코테/part02/chapter03/1이-될-때까지.py
@@ -6,8 +6,8 @@ cnt = 0
 while True:
   ## n이 k로 나눠질 때까지 1씩 빼기
   target = (n // k) * k
-  # n이 k보다 작을 때 반복문 탈출
-  if n < k:
+  # n이 1일 때 반복문 탈출
+  if n == 1:
     break
   cnt += (n - target)
   n = target
@@ -15,7 +15,5 @@ while True:
   cnt += 1
   n //= k
 
-# 마지막으로 남은 수에 대하여 1씩 빼기
-cnt += (n - 1)
 # 횟수 return
 print(cnt)
