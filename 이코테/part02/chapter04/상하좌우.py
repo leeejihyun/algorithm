8335eaa2ff5ddd2cdabb7f201f6434acb22108a2
leeejihyun97 <>
2022-01-05 09:04:46 +0000

    Update 상하좌우.py

--- 이코테/part02/chapter04/상하좌우.py
+++ 이코테/part02/chapter04/상하좌우.py
@@ -13,15 +13,16 @@ dy = [-1, 1, 0, 0]
 moveTypes = ['L', 'R', 'U', 'D']
 
 # plan에 따라 좌표 움직이기
-# 단, 정사각형 공간을 벗어나는 움직임은 무시
 for plan in plans:
   for i in range(len(moveTypes)):
     if plan == moveTypes[i]:
-      x += dx[i]
-      y += dy[i]
-      if x < 1 or x > n or y < 1 or y > n:
-        x -= dx[i]
-        y -= dy[i]
+      nx = x + dx[i]
+      ny = y + dy[i]
+      # 단, 정사각형 공간을 벗어나는 움직임은 무시
+      if nx < 1 or nx > n or ny < 1 or ny > n:
+        continue
+      # 이동 수행
+      x, y = nx, ny
 
 # 최종 x, y 출력
 print(x, y)
