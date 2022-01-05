6f3823bea0de87b2e9aeb85152b8a715fc1495f2
leeejihyun97 <>
2022-01-05 09:01:41 +0000

    Update 상하좌우.py

--- 이코테/part02/chapter04/상하좌우.py
+++ 이코테/part02/chapter04/상하좌우.py
@@ -1,31 +1,27 @@
 # N 저장
 n = int(input())
 
-# plan 저장
-plan = input().split()
+# plans 저장
+plans = input().split()
 
 # x, y 초기화
 x, y = 1, 1
 
-# direction에 따라 좌표 움직이기
+# L, R, U, D에 따른 이동 방향
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+moveTypes = ['L', 'R', 'U', 'D']
+
+# plan에 따라 좌표 움직이기
 # 단, 정사각형 공간을 벗어나는 움직임은 무시
-for direction in plan:
-    if direction == 'L':
-        y -= 1
-        if y < 1 or y > n:
-            y += 1
-    elif direction == 'R':
-        y += 1
-        if y < 1 or y > n:
-            y -= 1
-    elif direction == 'U':
-        x -= 1
-        if x < 1 or x > n:
-            x += 1
-    elif direction == 'D':
-        x += 1
-        if x < 1 or x > n:
-            x -= 1
+for plan in plans:
+  for i in range(len(moveTypes)):
+    if plan == moveTypes[i]:
+      x += dx[i]
+      y += dy[i]
+      if x < 1 or x > n or y < 1 or y > n:
+        x -= dx[i]
+        y -= dy[i]
 
 # 최종 x, y 출력
 print(x, y)
