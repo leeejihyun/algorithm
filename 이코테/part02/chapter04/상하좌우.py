d0e8184f094674abc3469f2857dc140285d0bc7d
leeejihyun97 <>
2022-01-05 08:37:09 +0000

    Change the directory of 상하좌우.py

---
+++ 이코테/part02/chapter04/상하좌우.py
@@ -0,0 +1,31 @@
+# N 저장
+n = int(input())
+
+# plan 저장
+plan = input().split()
+
+# x, y 초기화
+x, y = 1, 1
+
+# direction에 따라 좌표 움직이기
+# 단, 정사각형 공간을 벗어나는 움직임은 무시
+for direction in plan:
+    if direction == 'L':
+        y -= 1
+        if y < 1 or y > n:
+            y += 1
+    elif direction == 'R':
+        y += 1
+        if y < 1 or y > n:
+            y -= 1
+    elif direction == 'U':
+        x -= 1
+        if x < 1 or x > n:
+            x += 1
+    elif direction == 'D':
+        x += 1
+        if x < 1 or x > n:
+            x -= 1
+
+# 최종 x, y 출력
+print(x, y)
