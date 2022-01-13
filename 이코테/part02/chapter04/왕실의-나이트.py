1741293c0401e43852598c0415117647a869a976
leeejihyun97 <>
2022-01-13 03:36:55 +0000

    Update 왕실의-나이트.py

--- 이코테/part02/chapter04/왕실의-나이트.py
+++ 이코테/part02/chapter04/왕실의-나이트.py
@@ -2,68 +2,21 @@
 location = input()
 
 # location을 row와 col로 분리
-col = location[0]
-row = location[1]
+col = ord(location[0]) - ord('a') + 1
+row = int(location[1])
 
-# rows와 cols 선언
-cols = "abcdefgh"
-rows = "12345678"
+# 나이트가 이동할 수 있는 8가지 steps 선언
+steps = [(-2,-1), (-2,1), (2,-1), (2,1), (1,2), (1,-2), (-1,2), (-1,-2)]
 
-# 나이트가 이동할 수 있는 경우의 수 cnt 선언
+# 나이트가 이동할 수 있는지 카운트 선언
 cnt = 0
 
-# rowIndex와 colIndex 찾기
-colIndex = cols.index(col)
-rowIndex = rows.index(row)
-
-# 수평으로 두 칸 이동한 뒤에 수직으로 한 칸 이동
-# 왼쪽으로 2번
-newColIndex = colIndex - 2
-if newColIndex >= 0:
-  ## 위로 1 번
-  newRowIndex = rowIndex - 1
-  if newRowIndex >= 0:
-    cnt += 1
-  ## 아래로 1번
-  newRowIndex = rowIndex + 1
-  if newRowIndex < len(rows):
-    cnt += 1
-
-# 오른쪽으로 2번
-newColIndex = colIndex + 2
-if newColIndex < len(cols):
-  ## 위로 1번
-  newRowIndex = rowIndex - 1
-  if newRowIndex >= 0:
-    cnt += 1
-  ## 아래로 1번
-  newRowIndex = rowIndex + 1
-  if newRowIndex < len(rows):
-    cnt += 1
-
-# 수직으로 두 칸 이동한 뒤에 수평으로 한 칸 이동
-# 위로 2번
-newRowIndex = rowIndex - 2
-if newRowIndex >= 0:
-  ## 왼쪽으로 1 번
-  newColIndex = colIndex - 1
-  if newColIndex >= 0:
-    cnt += 1
-  ## 오른쪽으로 1번
-  newColIndex = colIndex + 1
-  if newColIndex < len(cols):
-    cnt += 1
-
-# 아래로 2번
-newRowIndex = rowIndex + 2
-if newRowIndex < len(rows):
-  ## 왼쪽으로 1 번
-  newColIndex = colIndex - 1
-  if newColIndex >= 0:
-    cnt += 1
-  ## 오른쪽으로 1번
-  newColIndex = colIndex + 1
-  if newColIndex < len(cols):
+# steps에 따라 이동
+for step in steps:
+  newRow = row + step[0]
+  newCol = col + step[1]
+  ## 해당 위치로 이동 가능하면 카운트 증가
+  if newRow >= 1 and newRow < 8 and newCol >= 1 and newCol < 8:
     cnt += 1
 
 # cnt 출력
